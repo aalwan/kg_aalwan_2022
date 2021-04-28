c12ed05a6cd8c3d3bc9aca6351b44b13924d693a
aalwan <83315113+aalwan@users.noreply.github.com>
2021-04-28 18:06:42 -0400

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,32 +1,52 @@
-var arr = [3,25,209];
-for(int i=0;i<length(arr);i++)
-{
-  var str = '';
-  for(int x = arr[i]; x > 0; x=(x-r)/10)
+<!DOCTYPE html>
+<html>
+<title>Web Page Design</title>
+<head>
+<script>
+  function solution(var arr)
   {
-    int r = x%10;
-    if(r == 0)
-      str = 'Zero'+str;
-    if(r == 1)
-      str = 'One'+str;
-    if(r == 2)
-      str = 'Two'+str;
-    if(r == 3)
-      str = 'Three'+str;
-    if(r == 4)
-      str = 'Four'+str;
-    if(r == 5)
-      str = 'Five'+str;
-    if(r == 6)
-      str = 'Six'+str;
-    if(r == 7)
-      str = 'Seven'+str;
-    if(r == 8)
-      str = 'Eight'+str;
-    if(r == 9)
-      str = 'Nine'+str;
+    var output = '';
+    for(int i=0;i<length(arr);i++)
+    {
+      var str = '';
+      for(int x = arr[i]; x > 0;)
+      {
+        int r = x%10;
+        if(r == 0)
+          str = 'Zero'+str;
+        if(r == 1)
+          str = 'One'+str;
+        if(r == 2)
+          str = 'Two'+str;
+        if(r == 3)
+          str = 'Three'+str;
+        if(r == 4)
+          str = 'Four'+str;
+        if(r == 5)
+          str = 'Five'+str;
+        if(r == 6)
+          str = 'Six'+str;
+        if(r == 7)
+          str = 'Seven'+str;
+        if(r == 8)
+          str = 'Eight'+str;
+        if(r == 9)
+          str = 'Nine'+str;
+        x=(x-r)/10;
+      }
+      if(i!=length(arr)-1)
+        str += ', ';
+      output += str;
+    }
+  console.log(output);
   }
-  if(i!=length(arr)-1)
-    str += ', ';
-  document.write(str)
-}
+
+var arr = process.argv.slice(2);
+var arr2 = parseInt(arr);
+solution(arr2);
+
+</script>
+</head>
+<body>
+</body>
+</html>
